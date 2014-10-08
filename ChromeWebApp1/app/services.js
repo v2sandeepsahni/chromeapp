8415229dd793be2221ff6e06f3e84a265747b3b7
Sandeep Sahni <sandeep.sahni@in.v2solutions.com>
2014-10-09 00:40:37 +0530

    adding files

---
+++ ChromeWebApp1/app/services.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('services', services);
+
+    services.$inject = ['$http'];
+
+    function services($http) {
+        this.getData = getData;
+
+        function getData() { }
+    }
+})();
